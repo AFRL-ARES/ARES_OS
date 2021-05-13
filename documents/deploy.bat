d0c004b46ec25531cb4a2b354e97d3d9368afa28
DaylondHooperIST <66254479+DaylondHooperIST@users.noreply.github.com>
2021-05-13 09:18:59 -0400

    Delete deploy.bat

--- documents/deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Rem %1 %2 are the source and destination directories respectively
-robocopy %1\AresAdditiveAnalysisPlugin %2 /e
-robocopy %1\AresAdditiveDevicesPlugin %2 /e
-robocopy %1\AresAdditivePlanningPlugin %2 /e
-robocopy %1\ARESCore %2 /e
